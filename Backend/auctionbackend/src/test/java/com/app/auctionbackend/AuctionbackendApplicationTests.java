e472492bd2713dd1989b4e3989bf95dbb03661a2
AzraBecirevic <azra.becirevic1998@gmail.com>
2021-02-20 21:34:13 +0100

    Create simple ping endpoint

---
+++ Backend/auctionbackend/src/test/java/com/app/auctionbackend/AuctionbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.auctionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
